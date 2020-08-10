199ae4eea544b4933cdde21369c183732babdcb8
RAIDERS <31316107+PavelRunchev@users.noreply.github.com>
2020-08-10 17:09:50 +0300

    Remove double file!

--- JS Advanced - 2020/JS Advanced Object Composition/Lab/objectFactory.js
+++
@@ -1,8 +0,0 @@
-function objectFactory(input) {
-    let objects = {};
-    JSON.parse(input).forEach(obj => { for (let key in obj) objects[key] = obj[key]; });
-
-    return objects;
-}
-
-console.log(objectFactory('[{"canMove": true},{"canMove":true, "doors": 4},{"capacity": 5}]'));
